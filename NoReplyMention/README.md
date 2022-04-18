e9e795fd7ddc0a8ae04676d080a13c3d31fc7318
FifiTheBulldog <fifithebulldog@outlook.com>
2022-04-17 22:11:56 -0400

    More stuff for NRM, add license and readme

---
+++ NoReplyMention/README.md
@@ -0,0 +1,5 @@
+# NoReplyMention
+
+Disable reply pings by default. Ported from 
+[NoReplyMention](https://github.com/BetterDiscordPlugins/userscripts/blob/master/NRM.plugin.js) 
+for [BetterDiscord](https://betterdiscord.app).
--- NoReplyMention/src/index.ts
+++ NoReplyMention/src/index.ts
@@ -1,16 +1,24 @@
 import { Plugin, registerPlugin } from "enmity-api/plugins";
+import { getModuleByProps } from "enmity-api/module";
+import { create } from "enmity-api/patcher";
 
-const ExamplePlugin: Plugin = {
-  name: "ExamplePlugin",
-  commands: [],
+const patcher = create("no-reply-mention");
+
+const pendingReplyModule = getModuleByProps("createPendingReply");
+
+const NoReplyMention: Plugin = {
+  name: "NoReplyMention",
 
   onStart() {
-
+    patcher.before(pendingReplyModule, "createPendingReply", (_, args) => {
+      args[0].shouldMention = false;
+      return args;
+    });
   },
 
   onStop() {
-
+    patcher.unpatchAll();
   }
-}
+};
 
-registerPlugin(ExamplePlugin);
+registerPlugin(NoReplyMention);
