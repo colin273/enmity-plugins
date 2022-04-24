850bc2a33ae08581684cbb8261269153b9039e1f
FifiTheBulldog <fifithebulldog@outlook.com>
2022-04-24 11:46:06 -0400

    Add sticker support

--- Freemoji/src/index.ts
+++ Freemoji/src/index.ts
@@ -4,15 +4,18 @@ import { create } from "enmity-api/patcher";
 
 const patcher = create("freemoji");
 
+const { messages } = window.enmity.modules.common;
+const { sendStickers } = messages;
+
 const [
-  parserModule,
-  { getEmojiURL },
   usability,
+  { getEmojiURL },
+  { getStickerById },
   { getChannel }
 ] = bulk(
-  filters.byProps("parse", "unparse", "parsePreprocessor"),
-  filters.byProps("getEmojiURL"),
   filters.byProps("canUseEmojisEverywhere", "canUseAnimatedEmojis"),
+  filters.byProps("getEmojiURL"),
+  filters.byProps("getStickerById"),
   filters.byProps("getChannel")
 )
 
@@ -34,13 +37,14 @@ const Freemoji: Plugin = {
   name: "Freemoji",
 
   onStart() {
-    patcher.after(usability, "canUseEmojisEverywhere", () => true);
-    patcher.after(usability, "canUseAnimatedEmojis", () => true);
-    patcher.before(window.enmity.modules.common.messages, "sendMessage", (_, [channelId, message]) => {
+    // Patch emoji
+    patcher.instead(usability, "canUseEmojisEverywhere", () => true);
+    patcher.instead(usability, "canUseAnimatedEmojis", () => true);
+    patcher.before(messages, "sendMessage", (_, [channelId, message]) => {
       const channel = getChannel(channelId);
       message.validNonShortcutEmojis.forEach((e: Emoji, i: number) => {
         if (e.guildId !== channel.guild_id) {
-          e.size = 24;
+          e.size = 24; // getEmojiURL returns a size that is twice this value
           message.content = message.content.replace(
             `<${e.animated ? "a" : ""}:${e.name}:${e.id}>`,
             getEmojiURL(e).replace("webp", "png")
@@ -49,6 +53,27 @@ const Freemoji: Plugin = {
         }
       })
       message.validNonShortcutEmojis = message.validNonShortcutEmojis.filter((e: Emoji) => e);
+    });
+
+    // Patch stickers
+    patcher.instead(usability, "canUse StickersEverywhere", () => true);
+    const { sendStickers } = messages;
+    patcher.before(messages, "sendStickers", (_, args) => {
+      const channel = getChannel(args[0]);
+      const stickerUrls: string[] = [];
+      args[1].forEach((s: string, i: number) => {
+        const sticker = getStickerById(s);
+        if (sticker.guild_id !== channel.guild_id) {
+          stickerUrls.push(`https://media.discordapp.net/stickers/${s}?size=320`);
+          delete args[1][i];
+        }
+      })
+      args[1] = args[1].filter((s: string) => s);
+      if (args[1]) {
+        sendStickers(...args);
+      } else {
+        messages.sendMessage(args[0], { content: stickerUrls.join("\n") });
+      }
     });
   },
 
